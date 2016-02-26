fe068d18afb2d518e418d2ca2a49208563ebd51c
Trevor Roman <troman360@gmail.com>
2016-02-25 23:33:13 -0500

    Built database and added some sample content

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(64), index=True, unique=True)
+    last_name = db.Column(db.String(64), index=True, unique=True)
+    full_name = db.Column(db.String(64), index=True, unique=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User %r>' % (self.full_name)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100))
+    abstract = db.Column(db.String(1000))
+    body = db.Column(db.String(10000))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % (self.title)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
-from app import app
+from app import app, db, models
+from datetime import date
 
 # Map this view to this URL
 @app.route('/index')
@@ -8,16 +9,36 @@ from app import app
 
 # Output this content to the screen for mapped URLs
 def about():
-    user = {'name': 'Trevor'}
-    page = "About"
-    
-    # Invokes Jinja2 templating engine, part of Flask framework
-    return render_template('about.html', user=user, page=page)
+	post = models.Post.query.filter_by(title='About Me').first()
+	title = post.title
+	abstract = post.abstract
+	body = post.body
+	author = post.author
+	year = date.today().year
+
+	# Invokes Jinja2 templating engine, part of Flask framework
+	return render_template('post.html', title=title, abstract=abstract, body=body, author=author, year=year)
 
 @app.route('/blog')
 def blog():
-    user = {'name': 'Trevor'}
-    page = "Blog"
-    
-    # Invokes Jinja2 templating engine, part of Flask framework
-    return render_template('construction.html', user=user, page=page)
+	post = models.Post.query.filter_by(title='Under Construction').first()
+	title = post.title
+	abstract = post.abstract
+	body = post.body
+	author = post.author
+	year = date.today().year
+
+	# Invokes Jinja2 templating engine, part of Flask framework
+	return render_template('post.html', title=title, abstract=abstract, body=body, author=author, year=year)
+
+@app.route('/example')
+def example():
+	post = models.Post.query.filter_by(title='About Me').first()
+	title = post.title
+	abstract = post.abstract
+	body = post.body
+	author = post.author
+	year = date.today().year
+
+	# Invokes Jinja2 templating engine, part of Flask framework
+	return render_template('eaxmple_post.html', title=title, abstract=abstract, body=body, author=author, year=year)
