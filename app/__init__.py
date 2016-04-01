3e75f5bcda02fc28d8bb4c715f79d496fe951f94
Trevor Roman <troman360@gmail.com>
2016-03-31 23:42:09 -0400

    Forgot to add these to the last commit:

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,10 @@
 import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from config import basedir
+from config import basedir, SQLALCHEMY_DATABASE_URI
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from sqlalchemy import create_engine
 
 # Initialize flask application
 app = Flask(__name__)
@@ -14,5 +17,13 @@ app.config.from_object('config')
 
 # Initialize database
 db = SQLAlchemy(app)
+engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
+
+# Create empty admin interface
+admin = Admin(app, name='trevorroman.com', template_mode='bootstrap3')
 
 from app import views, models
+
+admin.add_view(ModelView(models.Post, db.session))
+admin.add_view(ModelView(models.User, db.session))
+admin.add_view(ModelView(models.Tag, db.session))
