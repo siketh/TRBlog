9f3e9341d6f1f5262663394f2ee9cb8e9241993e
Trevor Roman <troman360@gmail.com>
2016-03-31 23:41:18 -0400

    Rebuilt backend with ORM style SQLAlchemy. Implemented many-to-many model for tags

--- app/models.py
+++ app/models.py
@@ -1,19 +1,24 @@
 from app import db
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(64), index=True, unique=True)
-    last_name = db.Column(db.String(64), index=True, unique=True)
-    full_name = db.Column(db.String(64), index=True, unique=True)
-    nickname = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+Base = declarative_base()
 
-    def __repr__(self):
-        return '<User %r>' % (self.full_name)
+post_tag_table = Table('post_tag_table', Base.metadata,
+    Column('post_id', Integer, ForeignKey('post.id')),
+    Column('tag_id', Integer, ForeignKey('tag.id'))
+)
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+post_user_table = Table('post_user_table', Base.metadata,
+    Column('post_id', Integer, ForeignKey('post.id')),
+    Column('user_id', Integer, ForeignKey('user.id')))
+
+class Post(Base):
+    __tablename__ = 'post'
+    
+    id = Column(Integer, primary_key=True)
     title = db.Column(db.String(100))
     abstract = db.Column(db.String(1000))
     body = db.Column(db.String(10000))
@@ -22,5 +27,63 @@ class Post(db.Model):
     repo_url = db.Column(db.String(200))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+    tags = relationship(
+        "Tag",
+        secondary=post_tag_table,
+        back_populates="posts")
+
+    def __init__(self, title="", abstract="", body="", repo_url="", tags=[]):
+        self.title = title
+        self.abstract = abstract
+        self.body = body
+        self.created = datetime.now()
+        self.updated = self.created
+        self.repo_url = repo_url
+        self.user_id = None
+        self.tags = tags
+
     def __repr__(self):
-        return '<Post %r>' % (self.title)
+        return '<Post Title %r>' % (self.title)
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(64), index=True, unique=True)
+    last_name = db.Column(db.String(64), index=True, unique=True)
+    full_name = db.Column(db.String(64), index=True, unique=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    
+    posts = relationship(
+        'Post', 
+        backref='author', 
+        lazy='dynamic')
+
+    def __init__(self, first_name="", last_name="", full_name="", nickname="", email="", posts=[]):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.full_name = full_name
+        self.nickname = nickname
+        self.email = email
+        self.posts = posts
+
+    def __repr__(self):
+        return '<User Name %r>' % (self.full_name)
+
+class Tag(Base):
+    __tablename__ = 'tag'
+    
+    id = Column(Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+
+    posts = relationship(
+        "Post",
+        secondary=post_tag_table,
+        back_populates="tags")
+
+    def __init__(self, name=""):
+        self.name = name
+
+    def __repr__(self):
+        return '<Tag Name %r>' % (self.name)
--- db_repository/versions/002_migration.py
+++ db_repository/versions/002_migration.py
@@ -5,43 +5,15 @@ from migrate import *
 from migrate.changeset import schema
 pre_meta = MetaData()
 post_meta = MetaData()
-post = Table('post', post_meta,
-    Column('id', Integer, primary_key=True, nullable=False),
-    Column('title', String(length=100)),
-    Column('abstract', String(length=1000)),
-    Column('body', String(length=10000)),
-    Column('timestamp', DateTime),
-    Column('user_id', Integer),
-)
-
-user = Table('user', post_meta,
-    Column('id', Integer, primary_key=True, nullable=False),
-    Column('first_name', String(length=64)),
-    Column('last_name', String(length=64)),
-    Column('full_name', String(length=64)),
-    Column('nickname', String(length=64)),
-    Column('email', String(length=120)),
-)
-
 
 def upgrade(migrate_engine):
     # Upgrade operations go here. Don't create your own engine; bind
     # migrate_engine to your metadata
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
-    post_meta.tables['post'].columns['abstract'].create()
-    post_meta.tables['post'].columns['title'].create()
-    post_meta.tables['user'].columns['first_name'].create()
-    post_meta.tables['user'].columns['full_name'].create()
-    post_meta.tables['user'].columns['last_name'].create()
 
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
-    post_meta.tables['post'].columns['abstract'].drop()
-    post_meta.tables['post'].columns['title'].drop()
-    post_meta.tables['user'].columns['first_name'].drop()
-    post_meta.tables['user'].columns['full_name'].drop()
-    post_meta.tables['user'].columns['last_name'].drop()
