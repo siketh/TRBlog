37e17294b5a1d1dcdd3fbd4077d6723c3c9fd250
Trevor Roman <troman360@gmail.com>
2016-04-03 22:55:58 -0400

    Added markdown converter to body text. Added 404 error handling. Created 404 page with consistent style. Added views for about and contact. Added basic styling.

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from flaskext.markdown import Markdown
 
 # Initialize flask application
 app = Flask(__name__)
@@ -20,12 +21,15 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
 Session = sessionmaker(bind=engine)
+session = Session()
 
 # Create empty admin interface
 admin = Admin(app, name='trevorroman.com', template_mode='bootstrap3')
 
+Markdown(app)
+
 from app import views, models
 
-admin.add_view(ModelView(models.Post, db.session))
-admin.add_view(ModelView(models.User, db.session))
-admin.add_view(ModelView(models.Tag, db.session))
+admin.add_view(ModelView(models.Post, session))
+admin.add_view(ModelView(models.User, session))
+admin.add_view(ModelView(models.Tag, session))
--- app/views.py
+++ app/views.py
@@ -1,13 +1,34 @@
-from flask import render_template, flash, redirect, url_for
-from app import app, db, models, Session
-from datetime import datetime, date
-from .forms import PostForm, SelectForm
+from flask import render_template
+from app import app, models, Session
+from datetime import date
 
-# Map this view to this URL
-@app.route('/blog')
-def blog():
+@app.route('/about')
+def about():
 	session = Session()
-	posts = session.query(models.User).order_by(models.User.id)
+	posts = session.query(models.Post).filter(models.Post.tags.any(models.Tag.name.in_(['About'])))
 	year = date.today().year
 
 	return render_template('post.html', posts=posts, year=year)
+
+@app.route('/contact')
+def contact():
+	session = Session()
+	posts = session.query(models.Post).filter(models.Post.tags.any(models.Tag.name.in_(['Contact'])))
+	year = date.today().year
+
+	return render_template('post.html', posts=posts, year=year)
+
+@app.route('/blog')
+def blog():
+	session = Session()
+	posts = session.query(models.Post).filter(~models.Post.tags.any(models.Tag.name.in_(['About', 'Contact']))).order_by(models.Post.updated)
+	year = date.today().year
+
+	return render_template('post.html', posts=posts, year=year)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	year = date.today().year
+
+	return render_template('404.html', posts=[], year=year), 404
+	
