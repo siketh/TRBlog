17d1abfc91b84a6a3afd44807c9e2920805dd884
Trevor Roman <troman360@gmail.com>
2016-04-09 01:33:57 -0400

    Added embedded disqus comments.

--- app/static/js/tr.js
+++ app/static/js/tr.js
@@ -2,5 +2,16 @@ $(window).load(function() {
 	var current_url = window.location.href
 	if (current_url.indexOf("/blog/post/") != -1) { 
 		$('.post-body').show();
+		$('.show-comments-button').show();
 	}
+
+	$('.show-comments-button').click(function() {
+		if($('.post-comments').css('display') == 'none') {
+			$('.post-comments').show();
+			$('.post-comments').css('height', 'auto');
+		}
+		else {
+			$('.post-comments').hide();
+		}
+	});
 });
