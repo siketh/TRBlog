29e9e90c241fb7e11b5a593da8fe2aa0d6f06634
Trevor Roman <troman360@gmail.com>
2016-07-10 20:52:05 -0400

    Added support for interchangeable configs through environment variable

--- config.py
+++ config.py
@@ -4,23 +4,51 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# environment
-CSRF_ENABLED = True
-SECRET_KEY = 'temp-secret-key'
-DEV_MODE = True
-DEBUG_MODE = True
-LOGGING_ENABLED=True
+environment = os.environ.get('ENV')
 
-# prod
-# BASE_URL = 'http://trevorroman.com'
+if environment is None:
+    # environment
+    CSRF_ENABLED = True
+    SECRET_KEY = 'temp-secret-key'
+    DEV_MODE = True
+    DEBUG_MODE = True
+    LOGGING_ENABLED = True
+    TESTING = False
 
-# dev
-BASE_URL = 'http://localhost:5000'
+    # for RSS
+    BASE_URL = 'http://localhost:5000'
 
-# pagination
-POSTS_PER_PAGE = 5
+    # pagination
+    POSTS_PER_PAGE = 5
 
-# database
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # database
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+elif environment == 'DEV':
+    # database
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'dev.db')
+
+elif environment == 'TEST':
+    # environment
+    DEV_MODE = False
+    DEBUG_MODE = False
+    TESTING = True
+
+    # database
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test.db')
+
+elif environment == 'PROD':
+    # environment
+    DEV_MODE = False
+    DEBUG_MODE = False
+
+    # for RSS
+    BASE_URL = 'http://www.trevorroman.com'
+
+    # pagination
+    POSTS_PER_PAGE = 20
+
+    # database
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
