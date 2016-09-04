0cd7b44b25192109c576b0eea99f913cd8d1490b
Trevor Roman <troman360@gmail.com>
2016-09-04 13:32:46 -0400

    Created preliminary unit testing framework and tests

--- tests.py
+++ tests.py
@@ -2,18 +2,47 @@
 import os
 import unittest
 
-from app import app, db
+from app import app, db, user_datastore
+import config
 
 
 class TestCase(unittest.TestCase):
     def setUp(self):
-        os.environ['ENV'] = 'TEST'
+        print("Setting up test environment")
+
+        configuration = config.TestConfig()
+        app.config.from_object(configuration)
+
         self.app = app.test_client()
+
         db.create_all()
+        user_datastore.create_user(email='troman360@gmail.com', password='password')
+        db.session.commit()
+
+    def test_login_logout(self):
+        print("Testing login")
+
+        response = self.login('troman360@gmail.com', 'password')
+        assert "200" in response.status
+
+        print("Testing logout")
+
+        response = self.logout()
+        assert "200" in response.status
 
     def tearDown(self):
+        print("Tearing down test environment")
+
         db.session.remove()
         db.drop_all()
+        os.environ['ENV'] = 'DEV'
+
+    def login(self, email, password):
+        return self.app.post('/login', data=dict(email=email, password=password), follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
 
 if __name__ == '__main__':
     unittest.main()
